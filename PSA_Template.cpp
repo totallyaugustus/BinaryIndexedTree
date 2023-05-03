e006d073bf8fb97d93c5bb23b232adf865fe2607
totallyaugustus <70984477+totallyaugustus@users.noreply.github.com>
2023-05-02 23:32:26 -0400

    Create PSA_Template.cpp

---
+++ PSA_Template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+#define MAXN 100000
+#define ll long long
+
+struct PSA {
+	ll arr[MAXN + 1];
+	void init() {
+		for (int i = 1; i <= MAXN; i++) {
+			arr[i] += arr[i - 1];
+		}
+	}
+	ll query(int i) {
+		return arr[i];
+	}
+	ll query(int l, int r) {
+		return query(r) - query(l - 1);
+	}
+};
+
+int main() {
+	return 0;
+}
