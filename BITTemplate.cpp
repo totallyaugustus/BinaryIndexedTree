881bfdaf41da9026096c01838da1dd28b61dfe3c
totallyaugustus <70984477+totallyaugustus@users.noreply.github.com>
2023-05-02 23:27:42 -0400

    Update BITTemplate.cpp

--- BITTemplate.cpp
+++ BITTemplate.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 struct BIT {
     ll arr[MAXN + 1];
-    void update(ll i, ll v) {
+    void update(int i, ll v) {
         for (; i <= MAXN; i += (i & -i)) {
             arr[i] += v;
         }
     }
-    ll query(ll i) {
+    ll query(int i) {
         ll v = 0;
         for (; i > 0; i -= (i & -i)) {
             v += arr[i];
